e64392fec8d29d3f56c67aa6f7a995348c89980e
mihran9991 <mihran@simplytechnologies.net>
2020-05-10 20:18:27 +0400

    feat(spinners): added spinners on async actions

--- src/components/Dashboard/Forms.js
+++ src/components/Dashboard/Forms.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { List, Card } from "antd";
+import { List, Card, Spin } from "antd";
 
 import routeConstants from "../../constants/routeConstants";
 import { getForms } from "../../services/request/formService";
 
 function Forms() {
   const [forms, setForms] = useState([]);
+  const [isSpinnig, setIsSpinnig] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
+      setIsSpinnig(true);
       try {
         const { data } = await getForms();
 
         setForms(data);
       } catch {
         console.log("error occured");
+      } finally {
+        setIsSpinnig(false);
       }
     };
 
@@ -23,27 +27,29 @@ function Forms() {
   }, []);
 
   return (
-    <List
-      grid={{ gutter: 16, column: 4 }}
-      dataSource={forms}
-      renderItem={({ name, id }) => (
-        <List.Item>
-          <Link
-            to={{
-              pathname: `${routeConstants.DASHBOARD}${routeConstants.FORM_INSTANCES}`,
-              state: {
-                formData: {
-                  formId: id,
-                  title: name,
+    <Spin spinning={isSpinnig}>
+      <List
+        grid={{ gutter: 16, column: 4 }}
+        dataSource={forms}
+        renderItem={({ name, id }) => (
+          <List.Item>
+            <Link
+              to={{
+                pathname: `${routeConstants.DASHBOARD}${routeConstants.FORM_INSTANCES}`,
+                state: {
+                  formData: {
+                    formId: id,
+                    title: name,
+                  },
                 },
-              },
-            }}
-          >
-            <Card title={name}>Form content</Card>
-          </Link>
-        </List.Item>
-      )}
-    />
+              }}
+            >
+              <Card title={name}>Form content</Card>
+            </Link>
+          </List.Item>
+        )}
+      />
+    </Spin>
   );
 }
 export default Forms;
